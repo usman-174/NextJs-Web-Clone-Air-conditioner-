d1544e7bbaff0449760d66e450991102849bf653
usman-174 <hellmughal123@gmail.com>
2022-05-16 16:32:14 +0500

    Improved nav3

--- src/components/NavRing.jsx
+++ src/components/NavRing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 
 const NavRing = ({ isOpen, setisOpen }) => {
